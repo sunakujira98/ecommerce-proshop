28780ecec3d844a84c146b31f22782e03484d28c
Steven Theodore <steventheodore98@gmail.com>
2020-11-16 22:40:10 +0700

    lecture 2 part 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <h1>Welcome To Proshop</h1>
+          <HomeScreen></HomeScreen>
         </Container>
       </main>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
