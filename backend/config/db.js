97c26c4c4845e8257454af603041f7880680cab8
Steven Theodore <steventheodore98@gmail.com>
2020-11-19 15:30:47 +0700

    lecture 3 part 3

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 
 const connectDB = async () => {
   try {
@@ -8,9 +9,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.log(`Error: ${error.message}`)
+    console.log(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server running in process ${process.env.NODE_ENV} on port ${PORT}`
+    `Server running in process ${process.env.NODE_ENV} on port ${PORT}`.yellow
+      .bold
   )
 )
