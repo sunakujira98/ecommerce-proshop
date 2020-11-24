b854c59ab90739b2742269293b6445533878585d
Steven Theodore <steventheodore98@gmail.com>
2020-11-24 22:59:26 +0700

    finnaly berhasil mongodb sialan

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,15 +3,18 @@ import colors from 'colors'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
+    const conn = await mongoose.connect(
+      'mongodb+srv://steven:p5KbQo5nBQJWgxNG@ecommerce-proshop.zqsyk.mongodb.net/proshop?retryWrites=true&w=majority',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    )
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.underline.bold)
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
