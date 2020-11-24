412a70df5832ff608cfafcc6b731c476fefde392
Steven Theodore <steventheodore98@gmail.com>
2020-11-24 23:12:09 +0700

    asdaf

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import colors from 'colors'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(PROCESS.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
