e7d3437ef821909cf501cc8483749d0a0996f8de
Steven Theodore <steventheodore98@gmail.com>
2020-12-18 22:59:11 +0700

    jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc  Auth user & get token
 // @route  POST /api/users/login
@@ -14,11 +15,18 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401).send('Invalid email or password')
   }
 })
 
-export { authUser }
+// @desc  GET user profile
+// @route  GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('Success')
+})
+
+export { authUser, getUserProfile }
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,6 @@
+import jwt from 'jsonwebtoker'
+import User from '../models/userModel.js'
+
+const protect = async (req, res, next) => {
+  let token
+}
