7c7b55c0c89156e2a973e60b9cd68e2bbd0de261
Steven Theodore <steventheodore98@gmail.com>
2021-01-18 22:34:39 +0700

    Admin Screen Security Access

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -17,6 +17,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from '../constants/userConstants'
 import { MY_ORDER_LIST_RESET } from '../constants/orderConstants'
 
@@ -59,6 +60,7 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
   dispatch({ type: USER_DETAILS_RESET })
+  dispatch({ type: USER_LIST_RESET })
   dispatch({ type: MY_ORDER_LIST_RESET })
 }
 
