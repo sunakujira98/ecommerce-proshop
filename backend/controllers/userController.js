d67d5d70be0010491ea8db60dddba98e144489fb
Steven Theodore <steventheodore98@gmail.com>
2021-01-18 22:11:16 +0700

    Admin Middleware & Get users list

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -109,4 +109,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile }
+// @desc  GET all users
+// @route  GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,13 +4,15 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
+router.get('/', protect, admin, getUsers)
 router.post('/', registerUser)
 router.post('/login', authUser)
-router.get('/profile', protect, getUserProfile)
+router.get('/', protect, getUserProfile)
 router.put('/profile', protect, updateUserProfile)
 
 export default router
